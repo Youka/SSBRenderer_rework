7f38e54669c41dad926ddbbc8d44c54d7b090633
Youka <spanknebel.borken@t-online.de>
2015-05-27 08:23:31 +0200

    began directshow interface

--- src/plugins/FilterBase.hpp
+++ src/plugins/FilterBase.hpp
@@ -22,17 +22,8 @@ Permission is granted to anyone to use this software for any purpose, including 
 
 // Cross interface filter functions
 namespace FilterBase{
-	// Meta informations
-	const char* get_id();	// Vapoursynth only
-	const char* get_namespace();	// Vapoursynth only
-	const char* get_name();
-	const char* get_description();
-	const char* get_version();
-	const char* get_author();
-	const char* get_copyright();
+	// Data types for functions below
 	enum class ArgType{BOOL, INTEGER, FLOAT, STRING, NONE};
-	std::vector<std::pair<std::string, ArgType>> get_opt_args();	// Avisynth & Vapoursynth only
-	// Process
 	enum class ColorType{BGR, BGRX, BGRA, UNKNOWN};
 	struct VideoInfo{
 		int width, height;
@@ -49,13 +40,26 @@ namespace FilterBase{
 			const char* s;
 		};
 	};
+	// Meta informations
+	const char* get_id();	// Vapoursynth only
+	const char* get_namespace();	// Vapoursynth only
+	const void* get_filter_guid();	// DirectShow only
+	const void* get_filter_config_guid();	// DirectShow only
+	const char* get_name();
+	const wchar_t* get_namew();	// DirectShow only
+	const char* get_description();
+	const char* get_version();
+	const char* get_author();
+	const char* get_copyright();
+	std::vector<std::pair<std::string, ArgType>> get_opt_args();	// Avisynth & Vapoursynth only
+	// Process
 	void init(VideoInfo vinfo, std::vector<Variant> args, void** userdata) throw (const char*);	// Avisynth & Vapoursynth only
 	void init(VideoInfo vinfo, void** userdata) throw (const char*);	// VirtualDub only
 	bool init(const char* filename, void** userdata);	// CSRI only
 	bool init(std::istream& stream, void** userdata);	// CSRI only
 	void setup(decltype(VideoInfo::width) width, decltype(VideoInfo::height) height, decltype(VideoInfo::format) format, void** userdata);	// CSRI only
-        void deinit(void* userdata);
-        std::string gen_args_description(void* userdata);	// VirtualDub only
-        int request_config(HWND wnd, void** userdata);	// VirtualDub only
-        void filter_frame(unsigned char* image_data, int stride, unsigned long ms, void** userdata);
+	void deinit(void* userdata);
+	std::string gen_args_description(void* userdata);	// VirtualDub only
+	int request_config(HWND wnd, void** userdata);	// VirtualDub only
+	void filter_frame(unsigned char* image_data, int stride, unsigned long ms, void** userdata);
 }
--- src/plugins/dshow.cpp
+++ src/plugins/dshow.cpp
@@ -12,4 +12,115 @@ Permission is granted to anyone to use this software for any purpose, including 
     3. This notice may not be removed or altered from any source distribution.
 */
 
+// Windows 'Globally Unique Identifier'
+#include <initguid.h>
+#include <CGuid.h>
+// Windows streams
+#include <streams.h>
+
 #include "FilterBase.hpp"
+
+// Filter configuration interface
+interface IFilterConfig : public IUnknown{
+	virtual void* Configure(void*) = 0;
+};
+
+// TODO
+
+// Video filter
+class MyFilter : public CVideoTransformFilter, public IFilterConfig{
+	public:
+		// Create class instance
+		static CUnknown* CALLBACK CreateInstance(LPUNKNOWN unknown, HRESULT* result){
+
+			// TODO
+
+			return nullptr;
+		}
+		// Filter configuration
+		void* Configure(void* conf){
+
+			// TODO
+
+			return nullptr;
+		}
+};
+
+// Filter pins
+const AMOVIESETUP_MEDIATYPE sudPinTypes[] = {
+	// Support RGB24 colorspace
+	{
+		&MEDIATYPE_Video,
+		&MEDIASUBTYPE_RGB24
+	},
+	// Support RGB32 colorspace
+	{
+		&MEDIATYPE_Video,
+		&MEDIASUBTYPE_RGB32
+	}
+};
+
+const AMOVIESETUP_PIN sudpPins[] = {
+	{
+		L"Input",             // Pin string name
+		FALSE,                // Is it rendered
+		FALSE,                // Is it an output
+		FALSE,                // Are we allowed none
+		FALSE,                // And allowed many
+		&CLSID_NULL,          // Connects to filter
+		nullptr,                 // Connects to pin
+		2,                    // Number of media types
+		sudPinTypes          // Media types
+	},
+	{
+		L"Output",            // Pin string name
+		FALSE,                // Is it rendered
+		TRUE,                 // Is it an output
+		FALSE,                // Are we allowed none
+		FALSE,                // And allowed many
+		&CLSID_NULL,          // Connects to filter
+		nullptr,                 // Connects to pin
+		2,                    // Number of media types
+		sudPinTypes          // Media types
+	}
+};
+
+// Filter setup
+const AMOVIESETUP_FILTER sudFilter = {
+	reinterpret_cast<const CLSID*>(FilterBase::get_filter_guid()),         // Filter CLSID
+	FilterBase::get_namew(),       // Filter name
+	MERIT_DO_NOT_USE,       // Filter merit
+	2,                      // Number of pins
+	sudpPins                // Pin information
+};
+
+// Filter definition as COM objects
+CFactoryTemplate g_Templates[] = {
+	{
+		FilterBase::get_namew(),	// Filter name
+		reinterpret_cast<const CLSID*>(FilterBase::get_filter_guid()),		// Filter CLSID
+		MyFilter::CreateInstance,	// Filter instance creation
+		nullptr,		// Init routine
+		&sudFilter		// Filter setup
+	}
+};
+
+int g_cTemplates = sizeof(g_Templates) / sizeof(CFactoryTemplate);	// Number of COM objects
+
+// Register filter to server
+EXTERN_C __declspec(dllexport) HRESULT WINAPI DllRegisterServer(){
+	return AMovieDllRegisterServer2(TRUE);
+}
+
+// Unregister filter from server
+EXTERN_C __declspec(dllexport) HRESULT WINAPI DllUnregisterServer(){
+	return AMovieDllRegisterServer2(FALSE);
+}
+
+// Further DLL initializations for DirectShow
+extern "C" BOOL WINAPI DllEntryPoint(HINSTANCE, ULONG, LPVOID);
+
+// DLL entry point
+BOOL APIENTRY DllMain(HANDLE module, DWORD reason, LPVOID reserved){
+	return DllEntryPoint(reinterpret_cast<HINSTANCE>(module), reason, reserved);
+}
