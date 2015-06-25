bab094a4614da5210e25c8981dc28b8c6a75acdf
Youka <spanknebel.borken@t-online.de>
2015-06-25 12:28:24 +0200

    implemented other interfaces in plugin invert test

--- src/plugins/tests/invert.cpp
+++ src/plugins/tests/invert.cpp
@@ -59,21 +59,85 @@ namespace FilterBase{
 	const char* get_copyright(){
 		return "...";
 	}
+	namespace AVS{
+		std::vector<std::pair<std::string, ArgType>> get_args(){
+			std::cout << "AVS - Tried to get arguments." << std::endl;
+			return std::vector<std::pair<std::string, ArgType>>();
+		}
+		void init(VideoInfo vinfo, std::vector<Variant>, void** userdata) throw (const char*){
+			std::cout << "AVS - Tried to initialize: " << vinfo.width << "x" << vinfo.height;
+			switch(vinfo.format){
+				case ColorType::BGR: std::cout << " BGR"; break;
+				case ColorType::BGRA: std::cout << " BGRA"; break;
+				case ColorType::BGRX: std::cout << " BGRX"; break;
+				case ColorType::UNKNOWN: std::cout << " UNKNOWN"; break;
+			}
+			std::cout << ' ' << vinfo.fps << "fps #" << vinfo.frames << std::endl;
+			*userdata = new int(vinfo.height);
+		}
+		void filter_frame(unsigned char* image_data, int stride, unsigned long ms, void** userdata){
+			std::cout << "AVS - Filter on time: " << ms << "ms" << std::endl;
+			std::transform(image_data, image_data + ::abs(*(reinterpret_cast<int*>(*userdata))) * stride,
+					image_data,
+					[](unsigned char elem){return 255 - elem;});
+		}
+		void deinit(void* userdata){
+			std::cout << "AVS - Tried to close." << std::endl;
+			delete reinterpret_cast<int*>(userdata);
+		}
+	}
+	namespace VDub{
+		void init(void**) throw (const char*){
+			std::cout << "VDub - Tried to initialize.";
+		}
+		std::string gen_args_desc(void*){
+			std::cout << "VDub - Tried to generate arguments description.";
+			return "";
+		}
+		int request_config(HWND wnd, void**){
+			std::cout << "VDub - Tried to request configuration with window handle " << wnd << " .";
+			return 0;
+		}
+		void start(VideoInfo vinfo, void** userdata) throw (const char*){
+			std::cout << "VDub - Tried to start: " << vinfo.width << "x" << vinfo.height;
+			switch(vinfo.format){
+				case ColorType::BGR: std::cout << " BGR"; break;
+				case ColorType::BGRA: std::cout << " BGRA"; break;
+				case ColorType::BGRX: std::cout << " BGRX"; break;
+				case ColorType::UNKNOWN: std::cout << " UNKNOWN"; break;
+			}
+			std::cout << ' ' << vinfo.fps << "fps #" << vinfo.frames << std::endl;
+			*userdata = new int(vinfo.height);
+		}
+		void filter_frame(unsigned char* image_data, int stride, unsigned long start_ms, unsigned long end_ms, void** userdata){
+			std::cout << "VDub - Filter on time: " << start_ms << "ms -> " << end_ms << "ms" << std::endl;
+			std::transform(image_data, image_data + ::abs(*(reinterpret_cast<int*>(*userdata))) * stride,
+					image_data,
+					[](unsigned char elem){return 255 - elem;});
+		}
+		void end(void** userdata){
+			std::cout << "VDub - Tried to end." << std::endl;
+			delete reinterpret_cast<int*>(*userdata);
+		}
+		void deinit(void*){
+			std::cout << "VDub - Tried to close.";
+		}
+	}
 	namespace CSRI{
 		bool init(const char* filename, void** userdata){
-			std::cout << "Tried to load file: " << filename << std::endl;
-			*userdata = new int;
+			std::cout << "CSRI - Tried to load file: " << filename << std::endl;
+			*userdata = new int(0);
 			return true;
 		}
 		bool init(std::istream& stream, void** userdata){
-			std::cout << "### Tried to load data ###\n--------------------------\n";
+			std::cout << "### CSRI - Tried to load data ###\n--------------------------\n";
 			std::copy(std::istream_iterator<char>(stream), std::istream_iterator<char>(), std::ostream_iterator<char>(std::cout));
 			std::cout.flush();
-			*userdata = new int;
+			*userdata = new int(0);
 			return true;
 		}
 		void setup(VideoInfo vinfo, void** userdata){
-			std::cout << "Tried to setup: " << vinfo.width << "x" << vinfo.height;
+			std::cout << "CSRI - Tried to setup: " << vinfo.width << "x" << vinfo.height;
 			switch(vinfo.format){
 				case ColorType::BGR: std::cout << " BGR"; break;
 				case ColorType::BGRA: std::cout << " BGRA"; break;
@@ -84,14 +148,55 @@ namespace FilterBase{
 			*(reinterpret_cast<int*>(*userdata)) = vinfo.height;
 		}
 		void filter_frame(unsigned char* image_data, int stride, unsigned long ms, void** userdata){
-			std::cout << "Filter on time: " << ms << "ms" << std::endl;
-			std::transform(image_data, image_data + *(reinterpret_cast<int*>(*userdata)) * stride,
+			std::cout << "CSRI - Filter on time: " << ms << "ms" << std::endl;
+			std::transform(image_data, image_data + ::abs(*(reinterpret_cast<int*>(*userdata))) * stride,
 					image_data,
 					[](unsigned char elem){return 255 - elem;});
 		}
 		void deinit(void* userdata){
-			std::cout << "Tried to close." << std::endl;
+			std::cout << "CSRI - Tried to close." << std::endl;
 			delete reinterpret_cast<int*>(userdata);
+		}
+	}
+	namespace DShow{
+		void init(IFilterConfig*) throw (const char*){
+			std::cout << "DShow - Tried to initialize.";
+		}
+		void start(VideoInfo vinfo, IFilterConfig* config) throw (const char*){
+			std::cout << "DShow - Tried to start: " << vinfo.width << "x" << vinfo.height;
+			switch(vinfo.format){
+				case ColorType::BGR: std::cout << " BGR"; break;
+				case ColorType::BGRA: std::cout << " BGRA"; break;
+				case ColorType::BGRX: std::cout << " BGRX"; break;
+				case ColorType::UNKNOWN: std::cout << " UNKNOWN"; break;
+			}
+			std::cout << ' ' << vinfo.fps << "fps #" << vinfo.frames << std::endl;
+			int** ppheight = reinterpret_cast<int**>(config->LockData());
+			delete *ppheight;
+			*ppheight = new int(vinfo.height);
+			config->UnlockData();
+		}
+		void filter_frame(unsigned char* image_data, int stride, unsigned long start_ms, unsigned long end_ms, IFilterConfig* config){
+			std::cout << "DShow - Filter on time: " << start_ms << "ms -> " << end_ms << "ms" << std::endl;
+			int* pheight = reinterpret_cast<int*>(*config->LockData());
+			if(pheight){
+				int height = *pheight;
+				config->UnlockData();
+				std::transform(image_data, image_data + ::abs(height) * stride,
+						image_data,
+						[](unsigned char elem){return 255 - elem;});
+			}else
+				config->UnlockData();
+		}
+		void end(IFilterConfig* config){
+			std::cout << "DShow - Tried to end." << std::endl;
+			int** ppheight = reinterpret_cast<int**>(config->LockData());
+			delete *ppheight;
+			*ppheight = nullptr;
+			config->UnlockData();
+		}
+		void deinit(IFilterConfig*){
+			std::cout << "DShow - Tried to close.";
 		}
 	}
 }
