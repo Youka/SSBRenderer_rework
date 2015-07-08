384034ba82c61c88f153dfda19a283dc3c4da407
Youka <spanknebel.borken@t-online.de>
2015-07-08 12:05:03 +0200

    added variable arguments to cmake add_linker_flags function

--- cmake/LinkerFlags.cmake
+++ cmake/LinkerFlags.cmake
@@ -1,9 +1,12 @@
 # Adds flags to all linking types
-function(add_linker_flags new_flags)
-	# Iterate through linking flags for executables, modules, shared and static libraries
-	foreach(linker_flags
-		CMAKE_EXE_LINKER_FLAGS CMAKE_MODULE_LINKER_FLAGS CMAKE_SHARED_LINKER_FLAGS)
-		# Append new flags to current linking flags
-		set(${linker_flags} "${${linker_flags}} ${new_flags}" PARENT_SCOPE)
+function(add_linker_flags)
+	# Iterate through function arguments
+	foreach(arg ${ARGV})
+		# Iterate through linking flags for executables, modules, shared and static libraries
+		foreach(linker_flags
+			CMAKE_EXE_LINKER_FLAGS CMAKE_MODULE_LINKER_FLAGS CMAKE_SHARED_LINKER_FLAGS)
+			# Append new flags to current linking flags
+			set(${linker_flags} "${${linker_flags}} ${arg}" PARENT_SCOPE)
+		endforeach()
 	endforeach()
 endfunction()
