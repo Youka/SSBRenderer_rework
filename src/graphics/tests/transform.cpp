16833c4c3365aabfbd9e77f89d046788e4e1c92c
Youka <spanknebel.borken@t-online.de>
2015-07-09 02:45:54 +0200

    updated matrix transformation test for matrix inversion test (still fails with SSE)

--- src/graphics/tests/transform.cpp
+++ src/graphics/tests/transform.cpp
@@ -21,11 +21,12 @@ Permission is granted to anyone to use this software for any purpose, including 
 #include <stdexcept>
 
 int main(){
-	double vec[] = {1, 0, 0}, *vec_end = vec+(sizeof(vec)/sizeof(*vec)),
-		transformed_vec[] = {-3, 0, 0};
-	if(std::equal(GUtils::Matrix4x4d().scale(3, 1, 1).rotate_z(M_PI).transform3d(vec), vec_end, transformed_vec))
-		std::copy(vec, vec_end, std::ostream_iterator<double>(std::cout, " "));
-	else
-		throw std::logic_error("Transformation failed");
+	double vec[] = {1, 0, 0}, *vec_end = vec+(sizeof(vec)/sizeof(*vec));
+	GUtils::Matrix4x4d mat;
+	std::ostream_iterator<double> cout(std::cout, " ");
+	std::copy(mat.scale(3, 1, 1).rotate_z(M_PI).transform3d(vec), vec_end, cout);
+	if(!mat.invert())
+		throw std::logic_error("Matrix inversion failed");
+	std::copy(mat.transform3d(vec), vec_end, cout);
 	return 0;
 }
