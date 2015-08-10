81fce9edb59088b096b19ea7d5dde44a5910bbb4
Youka <spanknebel.borken@t-online.de>
2015-08-11 00:32:19 +0200

    completed necessary utf-8 functions

--- src/strings/utf8.hpp
+++ src/strings/utf8.hpp
@@ -14,18 +14,64 @@ Permission is granted to anyone to use this software for any purpose, including 
 
 #pragma once
 
+#include <string>
+#include <vector>
 #ifdef _WIN32
 	#include <windef.h>
 	#include <Stringapiset.h>
 #endif
 
 namespace Utf8{
+	static inline size_t clen(const std::string& s, const size_t pos){
+		const unsigned char c = s[pos];
+		if(c & 0x3<<6){
+			if(c & 0x1<<5){
+				if(c & 0x1<<4){
+					if(c & 0x1<<3){
+						if(c & 0x1<<2){
+							if(c & 0x1<<1){
+								if(c & 0x1)
+									return 8;
+								return 7;
+							}
+							return 6;
+						}
+						return 5;
+					}
+					return 4;
+				}
+				return 3;
+			}
+			return 2;
+		}
+		return 1;
+	}
+
+	static inline size_t slen(const std::string& s){
+		size_t n = 0, i;
+		for(i = 0; i < s.length(); i += clen(s, i))
+			n++;
+		if(i != s.length()) n--;	// Last character wasn't complete
+		return n;
+	}
+
+	static inline std::vector<std::string> chars(const std::string& s){
+		std::vector<std::string> tokens;
+		tokens.reserve(slen(s));
+		for(size_t i = 0, len; i < s.length(); i += len){
+			len = clen(s, i);
+			if(i+len <= s.length())
+				tokens.push_back(s.substr(i, len));
+		}
+		return tokens;
+	}
 #ifdef _WIN32
 	static inline std::wstring to_utf16(const std::string& s){
 		std::wstring ws(MultiByteToWideChar(CP_UTF8, 0x0, s.data(), s.size(), NULL, 0), L'\0');
 		MultiByteToWideChar(CP_UTF8, 0x0, s.data(), s.size(), const_cast<wchar_t*>(ws.data()), ws.length());
 		return ws;
 	}
+
 	static inline std::string from_utf16(const std::wstring& ws){
 		std::string s(WideCharToMultiByte(CP_UTF8, 0x0, ws.data(), ws.size(), NULL, 0, NULL, NULL), '\0');
 		WideCharToMultiByte(CP_UTF8, 0x0, ws.data(), ws.size(), const_cast<char*>(s.data()), s.length(), NULL, NULL);
